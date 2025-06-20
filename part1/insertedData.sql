5a4828623357792a988e7f4d9b56aed636996eee
RyanFTx <ryanftyan@gmail.com>
2025-06-20 02:14:15 +0000

    [automatic commit] 2025-06-20 02:14:15 UTC

--- part1/insertedData.sql
+++ part1/insertedData.sql
@@ -12,7 +12,7 @@ INSERT INTO dogs (name, size, owner_id) VALUES
 ('Alyx','large',(SELECT id FROM users WHERE username = 'Gabenewell')),
 ('Speed','small',(SELECT id FROM users WHERE username = 'carol123'))
 
-INSERT INTO walk_requests (dog_id, requested_time, duration_minutes, location, status) VALUES
+INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status) VALUES
 /*
 sers:
 A user with the username alice123,
