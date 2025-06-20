51370894aba08b0e12d47a3f522afc5f4360ff05
RyanFTx <ryanftyan@gmail.com>
2025-06-20 08:39:21 +0000

    [automatic commit] 2025-06-20 08:39:21 UTC 14.203.54.57

--- part2/public/AJAX.js
+++ part2/public/AJAX.js
@@ -4,5 +4,8 @@ function sendLogin(){
         if (this.readyState === 4 && this.status === 200) {
             window.location.href = '/index.html';
         }
-        xhttp.open
+        xhttp.open('POST','/api/users/login',true);
+        xhttp.setRequestHeader('Content-Type', 'application/json');
+        xhttp.send(JSON.stringify({username: username, password: password}));
+    };
 }
