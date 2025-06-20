ddb8f7be50c3de2d72a4d2fe04d3e7c05eff9f17
RyanFTx <ryanftyan@gmail.com>
2025-06-20 01:50:22 +0000

    [automatic commit] 2025-06-20 01:50:22 UTC

--- part1/insertedData.sql
+++ part1/insertedData.sql
@@ -1,2 +1,10 @@
 INSERT INTO users (username, email. password_hash, role) VALUES
 ('alice123','')
+
+/*
+sers:
+A user with the username alice123,
+email alice@example.com, password hash hashed123, and role owner.
+A user with the username bobwalker, email bob@example.com, password hash hashed456, and role walker.
+A user with the username carol123, email carol@example.com, password hash hashed789, and role owner.
+Two more users with details of your choosing.
