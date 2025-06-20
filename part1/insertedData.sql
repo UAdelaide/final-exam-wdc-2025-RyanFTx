f7a43e88e04e2e97d86df3b4fbe4422eb2fa3702
RyanFTx <ryanftyan@gmail.com>
2025-06-20 02:34:48 +0000

    [automatic commit] 2025-06-20 02:34:48 UTC

--- part1/insertedData.sql
+++ part1/insertedData.sql
@@ -6,7 +6,7 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('Gabenewell','gaben@example.com','NumberAfter2','owner');
 
 INSERT INTO Dogs (name, size, owner_id) VALUES
-('Max','medium',(SELECT id FROM Users WHERE username = 'alice123')),
+('Max','medium',(SELECT user_idid FROM Users WHERE username = 'alice123')),
 ('Bella','small',(SELECT id FROM Users WHERE username = 'carol123')),
 ('Ledog','medium',(SELECT id FROM Users WHERE username = 'Lebronjames')),
 ('Alyx','large',(SELECT id FROM Users WHERE username = 'Gabenewell')),
@@ -14,10 +14,10 @@ INSERT INTO Dogs (name, size, owner_id) VALUES
 
 INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, status) VALUES
 ((SELECT dog_id FROM Dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
-((SELECT dog_idid FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
-((SELECT dog_idid FROM Dogs WHERE name = 'Ledog'), '2025-08-11 10:00:00', 60, 'Hindley St', 'open'),
-((SELECT dog_idid FROM Dogs WHERE name = 'Alyx'), '2025-08-12 11:00:00', 15, 'Torrens River', 'accepted'),
-((SELECT dog_idid FROM Dogs WHERE name = 'Speed'), '2025-08-13 12:00:00',25, 'Gotham City', 'open');
+((SELECT dog_id FROM Dogs WHERE name = 'Bella'), '2025-06-10 09:30:00', 45, 'Beachside Ave', 'accepted'),
+((SELECT dog_id FROM Dogs WHERE name = 'Ledog'), '2025-08-11 10:00:00', 60, 'Hindley St', 'open'),
+((SELECT dog_id FROM Dogs WHERE name = 'Alyx'), '2025-08-12 11:00:00', 15, 'Torrens River', 'accepted'),
+((SELECT dog_id FROM Dogs WHERE name = 'Speed'), '2025-08-13 12:00:00',25, 'Gotham City', 'open');
 
 
 
