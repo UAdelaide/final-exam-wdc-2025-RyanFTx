178bc4ece9250b858290c915729e5f49942f3868
RyanFTx <ryanftyan@gmail.com>
2025-06-20 02:31:27 +0000

    [automatic commit] 2025-06-20 02:31:27 UTC

--- part1/insertedData.sql
+++ part1/insertedData.sql
@@ -22,13 +22,4 @@ INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, location, st
 
 
 
-/*
-sers:
-A user with the username alice123,
-email alice@example.com,
-password hash hashed123, and role owner.
-A user with the username bobwalker,
-email bob@example.com,
-password hash hashed456,and role walker.
-A user with the username carol123, email carol@example.com, password hash hashed789, and role owner.
-Two more users with details of your choosing.
+
