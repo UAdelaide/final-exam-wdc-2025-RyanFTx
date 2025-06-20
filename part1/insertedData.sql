cfdf28aa530341fbeb2a5cefa07a5b75394befc9
RyanFTx <ryanftyan@gmail.com>
2025-06-20 01:56:24 +0000

    [automatic commit] 2025-06-20 01:56:24 UTC

--- part1/insertedData.sql
+++ part1/insertedData.sql
@@ -2,7 +2,8 @@ INSERT INTO users (username, email. password_hash, role) VALUES
 ('alice123','alice@example.com','hased123','owner')
 ('bobwalker','bob@example..com','hashed456', 'walker')
 ('carol123','carol@example.com','hashed789','owner')
-('LebronJames','lemail@example.com','Lepasswor')
+('LebronJames','lemail@example.com','LeHashed','owner')
+('')
 /*
 sers:
 A user with the username alice123,
