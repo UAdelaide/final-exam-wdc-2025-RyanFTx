034ed5ad697280670b6560eb6a629e53c40bdc29
RyanFTx <ryanftyan@gmail.com>
2025-06-20 02:22:59 +0000

    [automatic commit] 2025-06-20 02:22:59 UTC

--- part1/insertedData.sql
+++ part1/insertedData.sql
@@ -3,14 +3,14 @@ INSERT INTO users (username, email. password_hash, role) VALUES
 ('bobwalker','bob@example..com','hashed456', 'walker'),
 ('carol123','carol@example.com','hashed789','owner'),
 ('Lebronjames','lemail@example.com','LeHashed','owner'),
-('Gabenewell','gaben@example.com','NumberAfter2','owner')
+('Gabenewell','gaben@example.com','NumberAfter2','owner');
 
 INSERT INTO dogs (name, size, owner_id) VALUES
 ('Max','medium',(SELECT id FROM users WHERE username = 'alice123')),
 ('Bella','small',(SELECT id FROM users WHERE username = 'carol123')),
 ('Ledog','medium',(SELECT id FROM users WHERE username = 'Lebronjames')),
 ('Alyx','large',(SELECT id FROM users WHERE username = 'Gabenewell')),
-('Speed','small',(SELECT id FROM users WHERE username = 'carol123'))
+('Speed','small',(SELECT id FROM users WHERE username = 'carol123'));
 
 INSERT INTO WalkRequests(dog_id, requested_time, duration_minutes, location, status) VALUES
 ((SELECT id FROM dogs WHERE name = 'Max'), '2025-06-10 08:00:00', 30, 'Parklands', 'open'),
