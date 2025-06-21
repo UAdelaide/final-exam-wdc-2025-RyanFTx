f400b8d01dd46e2a86ed50acd8af7c98640d52a3
RyanFTx <ryanftyan@gmail.com>
2025-06-21 04:57:46 +0000

    [automatic commit] 2025-06-21 04:57:46 UTC 58.178.225.87

--- part1/db.js
+++ part1/db.js
@@ -15,7 +15,7 @@ async function getDogs(){
 }
 
 async function getWalkRequests(){
-    const [rows] = await pool.query('SELECT WalkRequests.request_id, Dogs.name, WalkRequests.requested_time, WalkRequests.duration_minutes, 
+    const [rows] = await pool.query('SELECT WalkRequests.request_id, Dogs.name, WalkRequests.requested_time, WalkRequests.duration_minutes, WalkRequests.location, Users.name as owner_username 
 }
 
 getDogs();
