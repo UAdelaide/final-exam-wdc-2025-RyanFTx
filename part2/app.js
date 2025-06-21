0fa4573b377d589052974ef85789f8b8f0163e1c
RyanFTx <ryanftyan@gmail.com>
2025-06-21 06:18:29 +0000

    [automatic commit] 2025-06-21 06:18:29 UTC 58.178.225.87

--- part2/app.js
+++ part2/app.js
@@ -37,11 +37,11 @@ app.use('/api/users', userRoutes);
 
 app.get('/owner-dashboard.html', authentication('owner'), (req,res) => {
     res.sendFile(path.join(__dirname, 'public', 'owner-dashboard.html'));
-}); //
+}); //Listens to get requests and runs middleware to check if user is logged in and has an owner role
 
 app.get('/walker-dashboard.html', authentication('walker'), (req,res) => {
     res.sendFile(path.join(__dirname, 'public', 'walker-dashboard.html'));
-});
+}); //same with above but just with the walker role
 
 app.use(express.static(path.join(__dirname, 'public')));
 // Export the app instead of listening here
