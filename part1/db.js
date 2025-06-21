b2ad624cb2c0585eb264d6bb2e7e152e661aa795
RyanFTx <ryanftyan@gmail.com>
2025-06-21 05:12:57 +0000

    [automatic commit] 2025-06-21 05:12:57 UTC 58.178.225.87

--- part1/db.js
+++ part1/db.js
@@ -20,7 +20,7 @@ async function getWalkRequests(){
 }
 
 async function getWalkerSummary(){
-    const [rows] = await pool.query('SELECT User.username AS walker_username, COUNT(r) ')
+    const [rows] = await pool.query('SELECT User.username AS walker_username, COUNT(rating) ')
 }
 
 
