294e93c21d545a8adaa10e322f5ba2d88a8c76e8
RyanFTx <ryanftyan@gmail.com>
2025-06-21 06:10:44 +0000

    [automatic commit] 2025-06-21 06:10:44 UTC 58.178.225.87

--- part2/public/AJAX.js
+++ part2/public/AJAX.js
@@ -3,8 +3,8 @@ function sendLogin(){
     const password = document.getElementById("password").value; //gets the input for username and pass for the site
     var xhttp = new XMLHttpRequest(); //creates new XMLHttp request
     xhttp.onreadystatechange = function() {
-        if (this.readyState === 4 && this.status === 200) { //if the ready state if 4 
-            document.getElementById("login-overlay").style.display = "none";
+        if (this.readyState === 4 && this.status === 200) { //if the ready state if 4 and status 200 (meaning there were no errors)
+            document.getElementById("login-overlay").style.display = "none"; //set login overlay to none
         }
     };
     xhttp.open('POST','/api/users/login',true);
